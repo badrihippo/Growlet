4d510bf465934982c24e688dc49dbaa5562ee588
Hippo <badrihippo@gmail.com>
2017-11-16 15:48:31 +0530

    Implement book detail view

---
+++ app/detail/book-detail-page.js
@@ -0,0 +1,28 @@
+const frameModule = require("ui/frame");
+
+const BookViewModel = require("./book-view-model");
+
+/* ***********************************************************
+* Use the "onNavigatingTo" handler to initialize the page binding context.
+*************************************************************/
+function onNavigatingTo(args) {
+    /* ***********************************************************
+    * The "onNavigatingTo" event handler lets you detect if the user navigated with a back button.
+    * Skipping the re-initialization on back navigation means the user will see the
+    * page in the same data state that he left it in before navigating.
+    *************************************************************/
+    if (args.isBackNavigation) {
+        return;
+    }
+
+    const page = args.object;
+    page.bindingContext = new BookViewModel(page.navigationContext.document);
+}
+
+function onBackButtonTap(args) {
+  console.log("Going back...");
+  frameModule.topmost().goBack();
+}
+
+exports.onNavigatingTo = onNavigatingTo;
+exports.onBackButtonTap = onBackButtonTap;
---
+++ app/detail/book-view-model.js
@@ -0,0 +1,11 @@
+const observableModule = require("data/observable");
+const observableArrayModule = require("data/observable-array");
+const couchbaseService = require("../shared/db/database.js");
+
+function BookViewModel(document) {
+    const viewModel = observableModule.fromObject(document);
+
+    return viewModel;
+}
+
+module.exports = BookViewModel;
