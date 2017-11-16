3ebf1b75cd0c44a650eb04b3bc51fd66703cab9c
Hippo <badrihippo@gmail.com>
2017-11-16 15:45:43 +0530

    Add Couchbase and implement data access

---
+++ app/shared/db/database.js
@@ -0,0 +1,50 @@
+var couchbase = require("nativescript-couchbase");
+
+var couchbaseService = function() {
+  this.db = new couchbase.Couchbase("testdb");
+  this.db.createView("books", "1", function (document, emitter) {
+    emitter.emit(document._id, document);
+  });
+}
+
+couchbaseService.prototype.saveBook = function(bookRecord) {
+  if (bookRecord.id) {
+    var documentId = db.updateDocument(bookRecord.id, {
+      title: bookRecord.title,
+      authors: bookRecord.authors,
+      genre: bookRecord.genre,
+      publisher: bookRecord.publisher,
+      isbn: bookRecord.isbn,
+      binding_type: bookRecord.binding_type,
+      price: bookRecord.price,
+      date_added: bookRecord.date_added,
+    });
+  } else {
+    var document = db.createDocument({
+      title: bookRecord.title,
+      authors: bookRecord.authors,
+      genre: bookRecord.genre,
+      publisher: bookRecord.publisher,
+      isbn: bookRecord.isbn,
+      binding_type: bookRecord.binding_type,
+      price: bookRecord.price,
+      date_added: bookRecord.date_added,
+    });
+  }
+};
+
+couchbaseService.prototype.getBookList = function() {
+  var bookList = [];
+  var rows = this.db.executeQuery("books");
+  for (var i = 0; i < rows.length; i++) {
+    bookList.push(rows[i]);
+  }
+  return bookList;
+}
+
+couchbaseService.prototype.getBookDetail = function(documentId) {
+  var document = this.db.getDocument(documentId);
+  return document;
+}
+
+module.exports = new couchbaseService();
